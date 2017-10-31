fda72cfaec70618bec635fe44c54456f6f61a5e3
Rubén Cárdenas <RCardenas@newgistics.com>
2017-10-30 19:33:11 -0600

    Added new firebase project, invisible to the public

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { firebaseConfig } from './environment';
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyBxokLeXiiln3hJ7eX7J1-TOt9ACaoJcnw",
-  authDomain: "recipe-manager-tikis.firebaseapp.com",
-  databaseURL: "https://recipe-manager-tikis.firebaseio.com",
-  projectId: "recipe-manager-tikis",
-  storageBucket: "recipe-manager-tikis.appspot.com",
-  messagingSenderId: "682387442525"
-};
 
 @NgModule({
   declarations: [
